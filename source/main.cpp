8be09394badc9892134bdec161b3b0d4db99a624
theorlangur <theorlangur@gmail.com>
2024-08-05 13:54:58 +0200

    initial commit

---
+++ source/main.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <string_view>
+#include <format>
+#include <charconv>
+#include <system_error>
+#include <complex>
+
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
